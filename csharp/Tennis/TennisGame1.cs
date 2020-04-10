ecb7e74a2dba591a159e26e632bfdd3a668c6a51
Miroslav Luckai <miroslav.luckai@gmail.com>
2020-04-10 16:04:34 +0200

    returns instead of help variable

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -23,34 +23,26 @@ namespace Tennis
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
             if (m_score1 == m_score2)
             {
                 if (m_score1 < 3)
                 {
                     return $"{IntScoreToString(m_score1)}-All";
                 }
-                return "Deuces";
+                return "Deuce";
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
                 var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                if (minusResult == 1) return "Advantage player1";
+                else if (minusResult == -1) return "Advantage player2";
+                else if (minusResult >= 2) return "Win for player1";
+                else return "Win for player2";
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    score += IntScoreToString(tempScore);
-                }
+                return $"{IntScoreToString(m_score1)}-{IntScoreToString(m_score2)}";
             }
-            return score;
         }
 
         private string IntScoreToString(int score)
