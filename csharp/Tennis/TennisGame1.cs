24a17fa2ed15450ddc82f268bd838bb12bd57cb1
Miroslav Luckai <miroslav.luckai@gmail.com>
2020-04-10 15:47:53 +0200

    Switch moved into private method, if edited to use this method

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -27,22 +27,11 @@ namespace Tennis
             var tempScore = 0;
             if (m_score1 == m_score2)
             {
-                switch (m_score1)
+                if (m_score1 < 3)
                 {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
+                    return $"{IntScoreToString(m_score1)}-All";
                 }
+                return "Deuces";
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
@@ -58,24 +47,27 @@ namespace Tennis
                 {
                     if (i == 1) tempScore = m_score1;
                     else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                    score += IntScoreToString(tempScore);
                 }
             }
             return score;
+        }
+
+        private string IntScoreToString(int score)
+        {
+            switch (score)
+            {
+                case 0:
+                    return "Love";
+                case 1:
+                    return "Fifteen";
+                case 2:
+                    return "Thirty";
+                case 3:
+                    return "Forty";
+                default:
+                    return string.Empty;
+            }
         }
     }
 }
